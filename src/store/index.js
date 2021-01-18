ce9d1af75a7dd2c5b14b618f17cd99e726cdc4a5
SI9INT <si9int@tuta.io>
2021-01-18 17:17:46 +0100

    axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Vue from "vue";
 import Vuex from "vuex";
 
@@ -5,14 +6,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    server: 'localhost:9000',
-    colSize: '',
-    rowSize: '',
-    firstGrid: '',
-    firstPlayer: '',
-    firstPlayerColor: '',
+    grid: [],
   },
-  mutations: {},
-  actions: {},
-  modules: {}
+  mutations: {
+    SET_GRID(state, grid) {
+      state.grid = grid
+    }
+  },
+  actions: {
+    getData({ commit }) {
+      axios.get('http://localhost:9000/json').then(response => {
+        commit('SET_GRID', response.data)
+      })
+    }
+  },
+  modules: {},
+  getters: {}
 });
